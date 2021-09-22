3d9b42b550163e8d828f19bf55c2549c9f0ae944
k.barysheva <In483die89Nacht>
2021-09-22 23:33:22 +0300

    rest assured tests for GET and Delete client by id

---
+++ src/test/java/com/acme/dbo/restassured/EndPoint.java
@@ -0,0 +1,8 @@
+package com.acme.dbo.restassured;
+
+public interface EndPoint {
+    String BASE_URL = "http://localhost";
+    int PORT = 8080;
+    String DBO_API = "dbo/api/";
+    String CLIENT_ID = "/client/{id}";
+}
